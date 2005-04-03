c5f7670fd16dccab507df1e181b358bf0a1b86fa
Ted Nathan Husted <husted@apache.org>
2005-04-03 15:59:00 +0000

    OVR-5 * Add test to demonstrate GUIDs (and to generate for starter database).
    git-svn-id: https://svn.apache.org/repos/asf/struts/sandbox/trunk@159928 13f79535-47bb-0310-9956-ffa450edef68

--- overdrive/PhoneBook/Test/BaseTest.cs
+++ overdrive/PhoneBook/Test/BaseTest.cs
@@ -1,14 +1,52 @@
+/*
+ * Copyright 2005 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+using System;
+using System.Collections;
 using NUnit.Framework;
 
 namespace PhoneBook.Core
 {
+	/// <summary>
+	/// Base class for unit tests.
+	/// </summary>
 	[TestFixture]
 	public class BaseTest
 	{
+		/// <summary>
+		/// Dummy test to exercise infrastructure.
+		/// </summary>
 		[Test]
 		public void Pass ()
 		{
 		}
 
+		/// <summary>
+		/// Demonstration GUIDs and provide a device for generating GUIDs if needed.
+		/// </summary>
+		[Test]
+		public void GuidString ()
+		{
+			IDictionary test = new Hashtable ();
+			for (int i = 0; i < 10; i++)
+			{
+				string key = Guid.NewGuid ().ToString ();
+				Assert.IsNotNull (key);
+				Assert.IsTrue (36 == key.Length);
+				test.Add (key, key); // Add throws an exception on duplicate keys
+			}
+		}
 	}
 }
