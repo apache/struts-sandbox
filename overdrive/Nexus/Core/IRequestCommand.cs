ba4349264dde59f7edf73cc43ff79e27657da29b
Ted Nathan Husted <husted@apache.org>
2005-04-08 23:47:26 +0000

    Add license statements to class files.
    git-svn-id: https://svn.apache.org/repos/asf/struts/sandbox/trunk@160638 13f79535-47bb-0310-9956-ffa450edef68

--- overdrive/Nexus/Core/IRequestCommand.cs
+++ overdrive/Nexus/Core/IRequestCommand.cs
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2005 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 using Agility.Core;
 
 namespace Nexus.Core
@@ -13,6 +28,15 @@ namespace Nexus.Core
 	/// </remarks>
 	public interface IRequestCommand : ICommand
 	{
+
+		/// <summary>
+		/// An identifier for this Command. 
+		/// </summary>
+		/// <remarks>
+		/// Corresponds to the Command property of IHelperContext. 
+		/// </remarks>
+		/// <returns>An identifier for this Command.</returns>
+		string ID {get; set;}
 
 		/// <summary>
 		/// Factory method to provide an empty context that can be used with the Command instance.
--- overdrive/Nexus/Test/App.cs
+++ overdrive/Nexus/Test/App.cs
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2005 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 namespace Nexus.Core
 {
 	/// <summary>
@@ -13,7 +28,7 @@ namespace Nexus.Core
 		/// <summary>
 		/// Token for first_name property.
 		/// </summary>
-		public const string LIST_ALL = "list_all";
+		public const string LIST_ALL = "ListAll";
 
 	}
 }
