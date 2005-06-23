58c4bfd00e1c4db2bb29dd4fdfee7a3481442336
Ted Nathan Husted <husted@apache.org>
2005-06-23 13:22:56 +0000

    OVR-5 * Add TelephoneProcessor to format telephone numbers in diretory list.
    git-svn-id: https://svn.apache.org/repos/asf/struts/sandbox/trunk@193137 13f79535-47bb-0310-9956-ffa450edef68

---
+++ overdrive/PhoneBook/Core/TelephoneProcessor.cs
@@ -0,0 +1,51 @@
+using System.Text;
+using Nexus.Core.Validators;
+
+namespace PhoneBook.Core
+{
+	/// <summary>
+	/// Remove punctuation on input and insert punctuation on output.
+	/// </summary>
+	public class TelephoneProcessor : Processor
+	{
+		public override bool ConvertInput (IProcessorContext incoming)
+		{
+			string source = incoming.Source as string;
+			if (source == null) return false;
+
+			char[] marks = {'-'};
+			string[] splits = source.Split (marks);
+			StringBuilder sb = new StringBuilder (source.Length);
+			foreach (string s in splits)
+			{
+				sb.Append (s);
+			}
+			incoming.Target = sb.ToString ();
+			return true;
+		}
+
+		public override bool FormatOutput (IProcessorContext outgoing)
+		{
+			string mark = "-";
+			string source = outgoing.Source as string;
+			if (source == null) return false;
+			string buffer = null;
+
+			if (source.Length == 10)
+			{
+				// 012-345-6789
+				string buffer1 = source.Insert (6, mark);
+				buffer = buffer1.Insert (3, mark);
+			}
+			else if (source.Length == 7)
+			{
+				// 012-3456
+				buffer = source.Insert (3, mark);
+			}
+			else buffer = source;
+
+			outgoing.Target = buffer;
+			return true;
+		}
+	}
+}
