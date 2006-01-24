04f9c3f3de70f62a4cd4af236b8144fd62a8c9b5
Wendy Smoak <wsmoak@apache.org>
2006-01-24 04:28:33 +0000

    Copied the Shale Blank webapp for conversion into a Maven 2 archetype.
    git-svn-id: https://svn.apache.org/repos/asf/struts/sandbox/trunk@371806 13f79535-47bb-0310-9956-ffa450edef68

---
+++ archetypes/shale-blank/src/java/org/apache/shale/blank/WelcomeBean.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2006 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * $Id$
+ */
+
+package org.apache.shale.blank;
+
+import java.util.Date;
+import org.apache.shale.view.AbstractViewController;
+
+/**
+ * <p>Sample <code>ViewController</code> class for <code>/welcome.jsp</code>.</p>
+ */
+public class WelcomeBean extends AbstractViewController {
+    
+
+    // -------------------------------------------------------------- Properties
+
+
+    /**
+     * <p>The current date and time value.</p>
+     */
+    private Date timestamp = null;
+
+
+    /**
+     * <p>Return the current date and time value.</p>
+     */
+    public Date getTimestamp() {
+
+        return this.timestamp;
+
+    }
+
+
+    /**
+     * <p>Set the current date and time value.</p>
+     *
+     * @param timestamp The new date and time value
+     */
+    public void setTimestamp(Date timestamp) {
+
+        this.timestamp = timestamp;
+
+    }
+
+
+    // --------------------------------------------------- ViewControler Methods
+
+
+    /**
+     * <p>Just before rendering occurs, set the <code>timestamp</code>
+     * property to the current date and time.</p>
+     */
+    public void prerender() {
+
+        setTimestamp(new Date());
+
+    }
+
+
+}
---
+++ archetypes/shale-blank/src/test/org/apache/shale/blank/WelcomeBeanTestCase.java
@@ -0,0 +1,107 @@
+/*
+ * Copyright 2006 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * $Id$
+ */
+
+package org.apache.shale.blank;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import org.apache.shale.test.base.AbstractViewControllerTestCase;
+
+/**
+ * <p>Test case for the {@link WelcomeBean} ViewController implementation.</p>
+ *
+ * $Id$
+ */
+public class WelcomeBeanTestCase extends AbstractViewControllerTestCase {
+
+
+    // ------------------------------------------------------------ Constructors
+
+
+    // Construct a new instance of this test case.
+    public WelcomeBeanTestCase(String name) {
+        super(name);
+    }
+
+
+    // ---------------------------------------------------- Overall Test Methods
+
+
+    // Set up instance variables required by this test case.
+    public void setUp() {
+
+        // Perform superclass setup tasks
+        super.setUp();
+
+        // Construct a new instance to be tested
+        bean = new WelcomeBean();
+
+        // Insert any additional setup for your unit test methods here
+
+    }
+
+    // Return the tests included in this test case.
+    public static Test suite() {
+
+        return (new TestSuite(WelcomeBeanTestCase.class));
+
+    }
+
+
+    // Tear down instance variables required by this test case.
+    public void tearDown() {
+
+        // Insert any additional teardown for your unit test methods here
+
+        // Remove the instance we just tested
+        bean = null;
+
+        // Perform superclass teardown tasks
+        super.tearDown();
+
+    }
+
+
+    // ------------------------------------------------------ Instance Variables
+
+
+    // The instance to be tested
+    WelcomeBean bean = null;
+
+
+    // ------------------------------------------------------------ Test Methods
+
+
+    // Test behavior of prerender() method
+    public void testPrerender() {
+
+        bean.prerender();
+        assertNotNull(bean.getTimestamp());
+
+    }
+
+
+    // Test a pristine instance of {@link WelcomeBean}
+    public void testPristine() {
+
+        assertNull(bean.getTimestamp());
+
+    }
+
+
+}
