e6ccb42fc0618ca775ff682cd119d548bef78a2e
Ted Nathan Husted <husted@apache.org>
2006-04-05 20:32:14 +0000

    mailreader-course/action2 * Add lab 1-1.
    git-svn-id: https://svn.apache.org/repos/asf/struts/sandbox/trunk@391792 13f79535-47bb-0310-9956-ffa450edef68

---
+++ mailreader-course/action2/lab-1-1-src/src/test/HomeTest.java
@@ -0,0 +1,16 @@
+import junit.framework.TestCase;
+
+/**
+ * An example text class to verify the configuration.
+ */
+public class HomeTest extends TestCase {
+
+    /**
+     * An example test that asserts true.
+     *
+     * @throws Exception On invalid assertions
+     */
+    public void testHomeAction() throws Exception {
+        assertTrue(true);
+    }
+}
---
+++ mailreader-course/action2/lab-1-1-src/src/webapp/pages/Home.jsp
@@ -0,0 +1,12 @@
+<%@ page contentType="text/html; charset=UTF-8" %>
+<%@ taglib prefix="ww" uri="/webwork" %>
+
+<html>
+<head>
+    <title>Home</title>
+</head>
+
+<body>
+<h2><ww:text name="home.nessage"/></h2>
+</body>
+</html>
