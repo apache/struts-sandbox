255d867a7d6585e034433c47e2fbb7c0137c23f4
Thomas Schneider <tschneider@apache.org>
2007-11-10 02:54:50 +0000

    renaming from juel to uel
    git-svn-id: https://svn.apache.org/repos/asf/struts/sandbox/trunk@593727 13f79535-47bb-0310-9956-ffa450edef68

---
+++ struts2-uel-plugin-example/src/main/java/example/SurveyAction.java
@@ -0,0 +1,30 @@
+package example;
+
+import java.util.Date;
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ * Simple Survey Action.
+ */
+public class SurveyAction extends ActionSupport {
+	private SurveyBean surveyBean = new SurveyBean();
+
+	public String edit() {
+		surveyBean.setAge(22);
+		surveyBean.setFirstName("Musachy");
+		surveyBean.setBirthdate(new Date());
+		return SUCCESS;
+	}
+
+	public String save() {
+		return SUCCESS;
+	}
+
+	public SurveyBean getSurveyBean() {
+		return surveyBean;
+	}
+
+	public void setSurveyBean(SurveyBean surveyBean) {
+		this.surveyBean = surveyBean;
+	}
+}
