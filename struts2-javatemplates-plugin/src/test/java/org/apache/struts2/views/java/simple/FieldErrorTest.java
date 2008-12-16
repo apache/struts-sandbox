f352df8f6d42270999a068354cf3687dc4f85a2d
David Newton <davenewton@apache.org>
2008-12-16 22:07:04 +0000

    Fixed testRenderFieldErrorWithoutFieldName test.
    git-svn-id: https://svn.apache.org/repos/asf/struts/sandbox/trunk@727190 13f79535-47bb-0310-9956-ffa450edef68

--- struts2-javatemplates-plugin/src/test/java/org/apache/struts2/views/java/simple/FieldErrorTest.java
+++ struts2-javatemplates-plugin/src/test/java/org/apache/struts2/views/java/simple/FieldErrorTest.java
@@ -64,7 +64,7 @@ public class FieldErrorTest extends AbstractTest {
         map.putAll(tag.getParameters());
         theme.renderTag(getTagName(), context);
         String output = writer.getBuffer().toString();
-        String expected = s("<ul class='errorMessage'><li><span>bad to the bone</span></li><li><span>not good</span></li><li><span>bad</span></li></ul>");
+        String expected = s("<ul class='errorMessage'><li><span>not good</span></li><li><span>bad</span></li><li><span>bad to the bone</span></li></ul>");
         assertEquals(expected, output);
     }
 
