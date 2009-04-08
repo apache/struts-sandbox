e975ca2da6e182a7f7c20c29789d1626fa396f1d
Musachy Barroso <musachy@apache.org>
2009-04-08 15:58:12 +0000

    Add license header to files
    git-svn-id: https://svn.apache.org/repos/asf/struts/sandbox/trunk@763294 13f79535-47bb-0310-9956-ffa450edef68

--- struts2-osgi-plugin/admin-bundle/src/main/java/org/apache/struts2/osgi/admin/actions/ShellAction.java
+++ struts2-osgi-plugin/admin-bundle/src/main/java/org/apache/struts2/osgi/admin/actions/ShellAction.java
@@ -1,14 +1,39 @@
+/*
+ * $Id$
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.apache.struts2.osgi.admin.actions;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
-import org.apache.struts2.osgi.admin.services.ServicesFactory;
-import org.apache.struts2.osgi.admin.services.shell.ShellService;
+import org.apache.struts2.osgi.DefaultBundleAccessor;
+import org.apache.felix.shell.ShellService;
+import org.osgi.framework.ServiceReference;
 
 import com.opensymphony.xwork2.Action;
 import com.opensymphony.xwork2.ActionSupport;
 
+/**
+ * This action executes commands on the Felix Shell
+ */
 public class ShellAction extends ActionSupport {
     private String command;
     private String output;
@@ -23,8 +48,7 @@ public class ShellAction extends ActionSupport {
         String outString = null;
         String errString = null;
         try {
-            ShellService shellService = ServicesFactory.getInstance().getShellService();
-            shellService.executeCommand(command, outStream, errStream);
+            executeCommand(command, outStream, errStream);
             outString = outByteStream.toString();
             errString = errByteStream.toString();
         } catch (Exception e) {
@@ -48,5 +72,20 @@ public class ShellAction extends ActionSupport {
 
     public String getOutput() {
         return output;
+    }
+
+    public void executeCommand(String commandLine, PrintStream out, PrintStream err) throws Exception {
+        ShellService shellService = getShellService();
+        if (shellService != null)
+            shellService.executeCommand(commandLine, out, err);
+        else
+            err.println("Apache Felix Shell service is not installed");
+    }
+
+    private ShellService getShellService() {
+        //bundle can be de-activated, so keeping a reference aorund is not a good idea
+        DefaultBundleAccessor bundleAcessor = DefaultBundleAccessor.getInstance();
+        ServiceReference ref = bundleAcessor.getServiceReference(ShellService.class.getName());
+        return (ShellService) bundleAcessor.getService(ref);
     }
 }
