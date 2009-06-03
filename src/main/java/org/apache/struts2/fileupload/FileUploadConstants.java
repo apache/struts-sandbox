ebf31bc3776cf997e0f78627f6fb171b392c9f1c
Wesley Wannemacher <wesw@apache.org>
2009-06-03 21:24:50 +0000

    first import, still need to create a struts-plugin.xml file that maps the beans.
    git-svn-id: https://svn.apache.org/repos/asf/struts/sandbox/trunk@781567 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/org/apache/struts2/fileupload/FileUploadConstants.java
@@ -0,0 +1,65 @@
+/*
+ * $Id$
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.struts2.fileupload;
+
+/**
+ * Holder for constants
+ *
+ * @author Wes Wannemacher
+ */
+public final class FileUploadConstants {
+
+    /**
+     *
+     */
+    public static final String PROGRESS_LISTENER = "struts.fileuploadplugin.progressListener";
+
+    /**
+     *
+     */
+    public static final String FILE_ITEM_FACTORY = "struts.fileuploadplugin.fileitemfactory";
+
+    /**
+     *
+     */
+    public static final String SIZE_THRESHOLD = "struts.fileuploadplugin.sizethreshold";
+
+    /**
+     *
+     */
+    public static final String FILE_REPOSITORY_PATH = "struts.fileuploadplugin.repositorypath";
+
+    /**
+     *
+     */
+    public static final String IS_PORTLET = "struts.fileuploadplugin.isportletupload";
+
+    /**
+     *
+     */
+    public static final String LISTENER_UPDATE_FREQUENCY = "struts.fileuploadplugin.listenerupdatefrequency";
+
+    /**
+     *
+     */
+    public static final String TIME_TO_KEEP_STATUS = "struts.fileupload.timetokeepstatus";
+
+}
