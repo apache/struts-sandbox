4d666b75b5c6f464d76f96ca06a9f823b727c34c
Musachy Barroso <musachy@apache.org>
2009-11-04 06:55:12 +0000

    add method el resolver
    git-svn-id: https://svn.apache.org/repos/asf/struts/sandbox/trunk@832667 13f79535-47bb-0310-9956-ffa450edef68

--- struts2-uel-plugin/src/main/java/org/apache/struts2/uelplugin/CompoundRootELContext.java
+++ struts2-uel-plugin/src/main/java/org/apache/struts2/uelplugin/CompoundRootELContext.java
@@ -1,29 +1,23 @@
 package org.apache.struts2.uelplugin;
 
-import javax.el.ArrayELResolver;
-import javax.el.CompositeELResolver;
-import javax.el.ELContext;
-import javax.el.ELResolver;
-import javax.el.FunctionMapper;
-import javax.el.ListELResolver;
-import javax.el.MapELResolver;
-import javax.el.ResourceBundleELResolver;
-import javax.el.VariableMapper;
+import javax.el.*;
 
 import org.apache.struts2.uelplugin.elresolvers.CompoundRootELResolver;
 import org.apache.struts2.uelplugin.elresolvers.XWorkBeanELResolver;
+import org.apache.struts2.uelplugin.elresolvers.PublicMethodResolver;
+import de.odysseus.el.util.SimpleContext;
 
 
 /**
  * An implementation of SimpleContext that knows about the ValueStack's
  * CompoundRoot.
  */
-public class CompoundRootELContext extends ELContext {
-    private ELResolver DEFAULT_RESOLVER_READ_WRITE;
-
+public class CompoundRootELContext extends SimpleContext {
     public CompoundRootELContext() {
-        DEFAULT_RESOLVER_READ_WRITE = new CompositeELResolver() {
+        super(new CompositeELResolver() {
             {
+                add(new BeanELResolver());
+                add(new PublicMethodResolver());
                 add(new CompoundRootELResolver());
                 add(new ArrayELResolver(false));
                 add(new ListELResolver(false));
@@ -31,17 +25,12 @@ public class CompoundRootELContext extends ELContext {
                 add(new ResourceBundleELResolver());
                 add(new XWorkBeanELResolver());
             }
-        };
+        });
     }
 
     @Override
     public VariableMapper getVariableMapper() {
         return null;
-    }
-
-    @Override
-    public ELResolver getELResolver() {
-        return DEFAULT_RESOLVER_READ_WRITE;
     }
 
     @Override
